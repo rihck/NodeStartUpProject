3320edf25deb23de662bfb2ff055cc563537c569
rickhpdev <ricardophp29@gmail.com>
2018-06-04 21:18:43 -0300

    Isolating Express in Module and separating routes in external file too

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine','ejs');
-
-app.get('/products', (req, res) => {
-    res.render("products/list");
-});
+var app = require('./config/express')();
+var rotasProdutos = require('./app/routes/produtos')(app);
 
 var portToListen = process.env.PORT || 3000;
 app.listen(portToListen, () => console.log('Server Running! =)'));
