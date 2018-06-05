279508fe4ea35869428807c5a3eb9a19a7181be6
rickhpdev <ricardophp29@gmail.com>
2018-06-05 20:59:11 -0300

    Adding MySql to Application

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,21 @@
 module.exports = (app) => {
     app.get('/products', (req, res) => {
-        res.render("products/list");
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host : '127.0.0.1',
+            user : 'root',
+            password : '12345',
+            database : 'codehouse_nodejs'
+        });
+
+        connection.query('select * from books', (err, results) => {
+            if (err) console.log('Deu erro!');
+            
+            console.log(results);
+            res.send(results);
+        });
+
+        connection.end();
+        //res.render("products/list");
     });
 }
